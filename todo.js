912f889ae91cc26f811eb7bd67e1ba983c02e2c9
DSurguy <Surguy.Derek@gmail.com>
2019-04-11 22:14:44 -0500

    Update todo

--- todo.js
+++ todo.js
@@ -1,4 +1,3 @@
-//TODO: Auto-switch to new run on creation
 //TODO: Show "Run Started" event in viewer
 //TODO: Show prompt for "Select Starter" event
-// - don't actually implement this on this branch, it's a lot of work to select starter (have to bring in pokemon)
+// - don't actually implement the modal on this branch, it's a lot of work to select starter (have to bring in pokemon)
