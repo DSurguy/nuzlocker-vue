49493d1eaa8ac54cc5475132b4cddfb01d2a9905
DSurguy <Surguy.Derek@gmail.com>
2019-04-15 21:21:05 -0500

    Fix broken tests

--- src/components/modals/__tests__/ModalSelectStarter.spec.js
+++ src/components/modals/__tests__/ModalSelectStarter.spec.js
@@ -142,9 +142,8 @@ describe('ModalSelectStarter', function () {
       species,
       level,
       source: {
-        location: false,
-        event: true,
-        identifier: 'starter'
+        type: 'event',
+        id: 'starter'
       },
       outcome: {
         captured: true,
