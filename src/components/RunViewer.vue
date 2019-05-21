ebfac719771b23e7fc02feaad370bb30865ff4ee
DSurguy <Surguy.Derek@gmail.com>
2019-05-20 22:15:28 -0500

    Add menus to runViewer

--- src/components/RunViewer.vue
+++ src/components/RunViewer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="run-viewer">
-    <div class="runMeta" v-if="!error && !notFound">
+    <div class="run-meta" v-if="!error && !notFound">
       <div class="meta-name"><span test-label="runName">{{run.name}}</span></div>
       <div class="meta-game" test-label="runGame"></div>
       <div class="meta-status" test-label="runStatus"></div>
@@ -43,11 +43,71 @@
       v-bind:run="run"
       v-bind:onComplete="onSelectStarterComplete"
     />
-    <div class="runControls">
-      <div class="encounters button is-light">Encounters</div>
-      <div class="goals button is-light">Goals</div>
-      <div class="manage button is-light">Manage Run</div>
+    <div class="run-controls">
+      <div class="sub-menu" v-show="showSubMenu">
+        <button 
+          v-for="(button, index) in subMenuActions"
+          :key="index"
+          class="sub-button button is-dark"
+        >
+          <div class="sub-button-icon">
+            <i class="fas" :class="[subMenuActions[index].icon]"></i>
+          </div>
+          <div class="sub-button-text">
+            {{subMenuActions[index].text}}
+          </div>
+        </button>
+      </div>
+      <div class="main-menu">
+        <div 
+          class="main-button-container"
+          :class="{'active': activeSubMenu === 'encounters'}"
+        >
+          <button 
+            class="encounters button main-button"
+            :class="[activeSubMenu === 'encounters' ? 'is-dark' : 'is-light']"
+            :disabled="activeSubMenu === 'encounters'"
+            v-on:click="onMenuButtonClick('encounters')"
+            text-label="encountersButton"
+          >
+            Encounters
+          </button>
+        </div>
+        <div 
+          class="main-button-container"
+          :class="{'active': activeSubMenu === 'goals'}"
+        >
+          <button 
+            class="goals button main-button"
+            :class="[activeSubMenu === 'goals' ? 'is-dark' : 'is-light']"
+            :disabled="activeSubMenu === 'goals'"
+            v-on:click="onMenuButtonClick('goals')"
+            text-label="goalsButton"
+          >
+            Goals
+          </button>
+        </div>
+        <div 
+          class="main-button-container"
+          :class="{'active': activeSubMenu === 'manage'}"
+        >
+          <button 
+            class="manage button main-button"
+            :class="[activeSubMenu === 'manage' ? 'is-dark' : 'is-light']"
+            :disabled="activeSubMenu === 'manage'"
+            v-on:click="onMenuButtonClick('manage')"
+            text-label="manageButton"
+          >
+            Manage Run
+          </button>
+        </div>
+      </div>
     </div>
+    <div 
+      class="sub-menu-background" 
+      v-show="showSubMenu"
+      v-on:click="closeSubMenu"
+    ></div>
   </div>
 </template>
 
@@ -84,7 +144,10 @@ export default {
       notFound: null,
       error: null,
       showSelectStarter: false,
-      starterModalActive: false
+      starterModalActive: false,
+      activeSubMenu: false,
+      showSubMenu: false,
+      subMenuActions: []
     }
   },
   mounted: async function () {
@@ -123,6 +186,37 @@ export default {
         this.showSelectStarter = false;
       }
       this.starterModalActive = false;
+    },
+    onMenuButtonClick: function (menuCategory){
+      switch(menuCategory){
+        case 'encounters':
+          this.subMenuActions = [
+            { icon: 'fa-leaf', text: 'Field', action: "encounterField" },
+            { icon: 'fa-exclamation', text: 'Event', action: "encounterEvent" },
+            { icon: 'fa-list', text: 'Stats', action: "encounterStats" }
+          ]
+        break;
+        case 'goals':
+          this.subMenuActions = [
+            { icon: 'fa-gem', text: 'Update Goals', action: "goalsUpdate" },
+            { icon: 'fa-list-alt', text: 'Static View', action: "goalsStaticView" }
+          ]
+        break;
+        case 'manage':
+          this.subMenuActions = [
+            { icon: 'fa-database', text: 'PokeDex', action: "managePokeDex" },
+            { icon: 'fa-map-marked-alt', text: 'RouteDex', action: "manageRouteDex" },
+            { icon: 'fa-wrench', text: 'Edit Status', action: "manageEditStatus" }
+          ]
+        break;
+      }
+      this.activeSubMenu = menuCategory;
+      this.showSubMenu = true;
+    },
+    closeSubMenu: function (){
+      this.showSubMenu = false;
+      this.activeSubMenu = null;
+      this.subMenuActions = []
     }
   }
 }
@@ -135,13 +229,12 @@ export default {
   padding-top: 64px;
   box-sizing: border-box;
 }
-.runMeta {
+.run-meta {
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
   display: flex;
-  width: 100%;
   height: 64px;
   background-color: var(--var-color-grey-dark);
   color: #fff;
@@ -159,18 +252,72 @@ export default {
   display: flex;
   justify-content: center;
 }
-.runControls {
-  display: flex;
-  justify-content: center;
+.run-controls {
   position: fixed;
+  z-index: 500;
   bottom: 0;
   left: 0;
   right: 0;
-  padding: 8px 0;
-  background-color: var(--var-color-grey);
+  padding-top: 72px;
 }
-.runControls > * {
-  margin: 0 8px;
-  width: 120px;
+.run-controls .main-menu {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: var(--var-color-grey);
+  width: 100%;
+}
+.run-controls .main-button-container {
+  display: flex;
+  padding: 16px 0;
+  justify-content: center;
+  align-items: center;
+  width: 30%;
+  max-width: 200px;
+  height: 100%;
+}
+.run-controls .main-button-container.active {
+  background-color: var(--var-color-grey-darker);
+}
+.run-controls .main-button {
+  width: 100%;
+  margin-right: 3%;
+}
+.run-controls .main-button:last-child{
+  margin-right: 0;
+}
+.run-controls .sub-menu {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 72px;
+  background-color: var(--var-color-grey-darker);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.run-controls .sub-button{
+  width: 30%;
+  max-width: 200px;
+  height: 56px;
+  justify-content: center;
+  flex-direction: column;
+  margin-right: 3%;
+}
+.run-controls .sub-button:last-child{
+  margin-right: 0;
+}
+.run-controls .sub-button > div {
+  display: block;
+}
+.sub-menu-background {
+  z-index: 400;
+  background: rgba(255,255,255,0.5);
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 }
 </style>
